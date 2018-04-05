8581e5710e30b085865dd2c61033075818087c59
essameldeen <essameldeen.ibrahim.stdsw@cis.asu.edu.eg>
2018-04-06 00:06:09 +0200

    first

---
+++ app/src/main/java/com/example/toshiba/dwana/splachScreen.java
@@ -0,0 +1,13 @@
+package com.example.toshiba.dwana;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class splachScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splach_screen);
+    }
+}
